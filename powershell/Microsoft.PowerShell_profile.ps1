1662b26efd3db0d1898b14adf425ef6cceffee92
Matheus Bastos <matheus.mab432@gmail.com>
2023-05-19 08:44:43 -0300

    adding more commands and improving existing ones

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,15 @@
 Set-Alias -Name k -Value kubectl -Description 'Kubernetes kubectl shorthand'
 Set-Alias -Name dn -Value dotnet -Description '.NET dotnet shorthand'
 Set-Alias -Name sk -Value skaffold -Description 'Skaffold skaffold shorthand'
-# Usage: rfc -c componentName (will be capitalized)
+
+# Scaffolds
+
+# Usage: rfc -c name, name2 (will be capitalized)
 Set-Alias -Name rfc -Value New-Rfc-WithPath -Description 'React functional component'
-# Usage: rffc -c componentName (will be capitalized)
+# Usage: rffc -c name, name2 (will be capitalized)
 Set-Alias -Name rffc -Value New-Rffc-WithPath -Description 'React functional component with filename'
+# Usage: tsf -f file-name, file-name2
+Set-Alias -Name tsf -Value TsFile-WithTest -Description 'Create a typescript file with a test in /__tests__'
 
 
 function prompt {
@@ -20,21 +25,52 @@ function New-Files {
 
   New-Item -ItemType Directory -Path $DirectoryName | ForEach-Object {
       foreach ($file in $Files) {
-          New-Item -ItemType File -Path $_ -Name $file
+          New-Item -ItemType File -Path $DirectoryName -Name $file
       }
   }
 }
 
-function New-Rfc-WithPath {
-  param ([string] $ComponentName)
+function TsFile-WithTest {
+  param (
+      [string[]]$Files
+  )
 
-  New-React-Fc -c $ComponentName -WithSrc $true -WithComp $true
+  $testDir = '__tests__'
+
+  if (!(Test-Path $testDir)) {
+      New-Item -ItemType Directory -Path $testDir
+  }
+
+  foreach ($file in $Files) {
+      $fileName = $file + '.ts'
+      $testFileName = $file + '.test.ts'
+
+      Write-Host $fileName
+      Write-Host $testFile
+
+      New-Item -ItemType File -Path $testDir -Name $testFileName
+      New-Item -ItemType File -Name $fileName
+  }
+}
+
+function New-Rfc-WithPath {
+  param ([string[]] $ComponentNames)
+
+
+  foreach ($componentName in $ComponentNames) {
+    New-React-Fc -c $componentName -WithSrc $true -WithComp $true
+  }
 }
 
 function New-Rffc-WithPath {
-  param ([string] $ComponentName)
+  param (
+    [string[]] $ComponentNames,
+    [bool]$WithTest = $false
+  )
 
-  New-React-Ffc -c $ComponentName -WithSrc $true -WithComp $true
+  foreach ($componentName in $ComponentNames) {
+    New-React-Ffc -c $componentName -WithSrc $true -WithComp $true -WithTest $WithTest
+  }
 }
 
 
@@ -52,23 +88,40 @@ function New-React-Fc {
 
 function New-React-Ffc {
   param (
-    [string]$ComponentName
+    [string]$ComponentName,
+    [bool]$WithSrc = $false,
+    [bool]$WithComp = $false,
+    [bool]$WithTest = $false
     )
 
-    $normalized = Normalize-ComponentName -ComponentName $ComponentName -WithSrc $WithSrc -WithComp $WithComp
-    $compFile = $normalized + ".tsx"
+    $path = Normalize-ComponentName -ComponentName $ComponentName -WithSrc $WithSrc -WithComp $WithComp
+    $capitalized = Capitalize-FirstLetter -S $ComponentName
+    $tsxFile = $capitalized + '.tsx'
 
-    New-Files -DirectoryName $normalized -Files $compFile, "index.ts", "style.module.scss"
+    if ($WithTest) {
+      $testFile = $capitalized + '.test.tsx'
+    }
+
+    New-Files -DirectoryName $path -Files $tsxFile, $testFile, "index.ts", "style.module.scss"
+
+    $indexPath = $path + '/index.ts'
+    $indexContent = 'export * from ' +"'./"+ $capitalized + "';"
+
+    Set-Content -Path $indexPath -Value $indexContent
+}
+
+function Change-React-Folder-Structure {
+  param (
+    [string]$DirectoryName
+    )
+
+    Rename-IndexFiles -p $DirectoryName
+    Add-IndexFiles -p $DirectoryName
 }
 
 
 # Presets
 
-# Import the Chocolatey Profile that contains the necessary code to enable
-# tab-completions to function for `choco`.
-# Be aware that if you are missing these lines from your profile, tab completion
-# for `choco` will not function.
-# See https://ch0.co/tab-completion for details.
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
@@ -82,10 +135,9 @@ function Normalize-ComponentName {
     [bool]$WithComp = $false
     )
 
-
     $normalized = Capitalize-FirstLetter -S $ComponentName
 
-  if ($WithComp) {
+    if ($WithComp) {
       $normalized = 'components/' + $normalized
     }
 
@@ -104,4 +156,43 @@ function Capitalize-FirstLetter {
     $firstChar = $String.Substring(0, 1).ToUpper()
     $capitalizedString = $firstChar + $String.Substring(1)
     return $capitalizedString
+}
+
+# Function to rename any index.tsx files in any child directory of a given directory to {childDirectory}.tsx
+function Rename-IndexFiles {
+    param (
+        [string]$ParentDirectory
+    )
+
+    $childDirectories = Get-ChildItem -Path $ParentDirectory -Directory
+
+    foreach ($childDirectory in $childDirectories) {
+        $indexPath = Join-Path -Path $childDirectory.FullName -ChildPath "index.tsx"
+        $newPath = Join-Path -Path $childDirectory.FullName -ChildPath "$($childDirectory.Name).tsx"
+
+        if (Test-Path -Path $indexPath) {
+            Rename-Item -Path $indexPath -NewName $newPath
+            Write-Host "Renamed $indexPath to $newPath"
+        }
+    }
+}
+
+# Function that adds a 'index.ts' file in every subdirectory of a given directory with export
+function Add-IndexFiles {
+  param (
+    [string]$ParentDirectory
+  )
+
+  $directories = Get-ChildItem -Path $ParentDirectory -Directory
+
+  foreach ($directory in $directories) {
+    $indexFile = $directory.FullName + '\index.ts'
+    if (!(Test-Path $indexFile)) {
+      New-Item -ItemType File -Path $directory.FullName -Name 'index.ts'
+
+      $indexContent = 'export * from ' +"'./"+ $directory + "';"
+
+      Set-Content -Path $indexFile -Value $indexContent
+    }
+  }
 }
